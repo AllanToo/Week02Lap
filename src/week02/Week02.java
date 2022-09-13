a6d97f5ee24eca6fc54c993f843100debd83947a
Allan T <Allan T@10.0.0.200>
2022-09-12 19:24:57 -0600

    New Project Creation Week 02 Created a new Java Application

---
+++ src/week02/Week02.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week02;
+
+/**
+ *
+ * @author Allan T
+ */
+public class Week02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      testMethod();
+    }
+    public static void testMethod()
+    {
+        System.out.println("This is Week 02 Assignment");
+    }
+}
